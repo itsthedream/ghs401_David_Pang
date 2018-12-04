6a4fa1d190cda4fe206234b1fab84a4f6ba1371c
David Pang <david.pan@lvjusd.k12.ca.us>
2018-12-04 12:40:28 -0800

    Example git

--- nameArray.java
+++ nameArray.java
@@ -2,14 +2,14 @@ public class nameArray
 {
     public static void age () 
     {
-        int[] nameArray = new int[4];
-        for(int i=0; i<nameArray.length; i++)
-            {
-                name.Array[] = {17, 21, 51, 55) 
-                System.out.println(String.length + "'s age is" + name.Array[i]
+        //int[] nameArray = new int[4];
+        //for(int i=0; i<nameArray.length; i++)
+          //  {
+               // name.Array[] = {17, 21, 51, 55) 
+               // System.out.println(String.length + "'s age is" + name.Array[i]
             }
 
-    
+    System.out.println("oof");
     
     
     
